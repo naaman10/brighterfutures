13b561960b5c8e92b96a1dce0b8807f41f4bb872
Naaman hudson <naamanh10@gmail.com>
2026-03-12 16:19:03 +0000

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,15 @@ const nationalPark = National_Park({
 export const metadata: Metadata = {
   title: "Brighter Futures",
   description: "Client dashboard for sending emails",
+  icons: {
+    icon: [
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    shortcut: "/favicon/favicon.ico",
+    apple: "/favicon/apple-touch-icon.png",
+  },
+  manifest: "/favicon/site.webmanifest",
 };
 
 export default function RootLayout({
